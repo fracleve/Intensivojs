525f988de50d7ebbc0f7780c1058e29f3c21b885
Francisco Alencar <clevison.alencar@gmail.com>
2024-07-05 11:58:20 -0300

    Add files via upload

---
+++ Projeto intensivão2 .js/main.js
@@ -0,0 +1,132 @@
+
+
+/*1. Crie um script que exiba a mensagem Hello World em um alerta no navegador.
+2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+    
+    💡 Para saber o tipo de dado você pode usar o operador typeof
+    
+4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".*/
+
+
+/* primeiro */
+
+
+
+
+alert("Hello World")
+
+
+/* segundo */
+ alert (n1 = 35)
+ alert (n2 = 45)
+
+ 
+
+
+let second =( n1 + n2)
+
+alert(second)
+
+
+/* terceiro*/
+
+
+let v1=10
+let v2="10"
+let v3=v1 === v2
+let v4="Francisco"
+
+alert ("valor:" + v1 + " - Tipo: " + typeof(v1))
+alert ("valor:" + v2 + " - É uma string : " + typeof(v2))
+alert ("valor:" + v3 + " - É um booleano : " + typeof(v3))
+alert ("Nome:" + v4 + " - Tipo: " + typeof(v4))
+
+
+
+/* sexto */
+
+
+
+alert (n3 = 35)
+alert (n4 = 45)
+
+ 
+
+
+let sub =( n3 - n4)
+
+alert(sub)
+
+
+
+
+alert (n5 = 35)
+alert (n6 = 45)
+
+ 
+
+
+let mult =( n5 * n6)
+
+alert(mult)
+
+
+
+
+alert (n7 = 35)
+alert (n8 = 45)
+
+ 
+
+
+let div =( n7 / n8)
+
+alert (div)
+
+
+
+
+alert (firstNumber = 10)
+
+
+let restDiv = firstNumber 
+
+
+
+let = restDiv/2;
+    
+    if(restDiv%2 == 0){
+        alert("É um numero par");
+    }else {
+        alert("E um numero impar");
+    }
+
+
+
+
+
+alert (secondNumber = 15)
+
+
+let restodadiv = secondNumber
+
+let = restodadiv/2;
+    
+    if(restodadiv%2 == 0){
+        alert("É um numero par");
+    }else {
+        alert("E um numero impar");
+    }
+  
+
+    alert("concluido") 
+
+
+
